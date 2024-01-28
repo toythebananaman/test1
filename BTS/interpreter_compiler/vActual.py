d7add73f114f65581bfeaeed6a58da520584a616
Cowlobthebossy <119730284+CowBlob@users.noreply.github.com>
2024-01-28 12:58:10 +0100

    init

---
+++ BTS/interpreter_compiler/vActual.py
@@ -0,0 +1,5 @@
+# current version
+current_version = "0_0_1"
+
+# Splitting version number
+current_version_major, current_version_minor, current_version_patch = current_version.split('_')
---
+++ BTS/interpreter_compiler/vLEEZL/v0_0_1.py
@@ -0,0 +1,11 @@
+# Define the Lexer
+def lexer(i):
+    # Implement tokenization logic here
+    i = i*10
+    return i
+
+# Define the Parser
+def parser(o):
+    # Implement parsing logic here
+    o = o/100
+    return o
