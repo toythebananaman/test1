7bce4156eb4fdf96cbb5a4b642c7d2f2c9b18a65
Cowlobthebossy <toythebannaman@gmail.com>
2024-01-28 13:41:52 +0100

    new py file to test branch in ghithub

---
+++ BTS/interpreter_compiler/vLEEZL/v0_0_3.py
@@ -0,0 +1,11 @@
+# Define the Lexer
+def lexer(i):
+    # Implement tokenization logic here
+    i = 10
+    return i
+
+# Define the Parser
+def parser(o):
+    # Implement parsing logic here
+    o = 20
+    return o
